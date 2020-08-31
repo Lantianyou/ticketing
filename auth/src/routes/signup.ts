689f6c678dfbd140dc387d20fe231ffa62161c2c
Lantianyou <dtlantianyou@gmail.com>
2020-08-31 14:04:14 +0800

    password

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/users";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
@@ -14,17 +16,24 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("密码长度必须在4到20个字符之间"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    const { email, password } = req.body;
-    console.log("Creating a user");
-    throw new DatabaseConnectionError();
 
-    res.send({});
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
