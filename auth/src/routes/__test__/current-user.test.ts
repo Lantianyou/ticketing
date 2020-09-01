50dc2346f72155d195c23c32bf46e97601417242
Lantianyou <dtlantianyou@gmail.com>
2020-09-01 16:31:06 +0800

    test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -4,14 +4,7 @@ import { app } from "../../app";
 it("responds with details about the current user", async () => {
   const email = "test@email.com";
   const password = "password";
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email,
-      password,
-    })
-    .expect(201);
-  const cookie = authResponse.get("Set-Cookie");
+  const cookie = await global.signin();
 
   const response = await request(app)
     .get("/api/users/currentuser")
@@ -20,5 +13,13 @@ it("responds with details about the current user", async () => {
     .expect(200);
 
   expect(response.body.currentUser.email).toEqual(email);
-  expect(response.body.currentUser.password).toEqual(password);
+});
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toBeNull();
 });
