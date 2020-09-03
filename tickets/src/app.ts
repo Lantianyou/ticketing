2c6da49879584bde35b63365ecc480f785483293
Lantianyou <dtlantianyou@gmail.com>
2020-09-03 08:44:36 +0800

    tickets start

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,8 +2,8 @@ import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
 import { json } from "body-parser";
-
-import { errorHandler, NotFoundError } from "@lanxtianyou/common";
+import { newTicketRouter } from "./routes/new";
+import { errorHandler, NotFoundError, currentUser } from "@lanxtianyou/common";
 
 const app = express();
 app.set("trust proxy", true);
@@ -14,6 +14,9 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+app.use(currentUser);
+
+app.use(newTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,10 +1,23 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("has a route handler", async () => {});
+it("has a route handler", async () => {
+  const response = await request(app).post("/api/tickets").send({});
 
-it("only user signed in", async () => {});
+  expect(response.status).not.toEqual(404);
+});
 
+it("only user signed in", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+
+  expect(response.status).toEqual(401);
+});
+
+it("returns other than 401 if user not signed in", async () => {
+  const response = await request(app).post("/api/tickets").send({});
+
+  expect(response.status).not.toEqual(401);
+});
 it("returns error if invalid title", async () => {});
 
 it("returns error if invalid price", async () => {});
