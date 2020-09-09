3c91ee9fb2af8a0bd76e37814129cf2793af2068
Lantianyou <dtlantianyou@gmail.com>
2020-09-09 11:41:33 +0800

    ticket service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -10,16 +10,29 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error("No mongo url");
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("No NATS_CLIENT_ID");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("No NATS_URL");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("No NATS_CLUSTER_ID");
+  }
 
   try {
-    await natsWrapper.connect("ticketing", "asdf", "http://nats-srv:4222");
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed");
       process.exit();
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-    
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
