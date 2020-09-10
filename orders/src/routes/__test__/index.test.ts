c7be52015f610ffd8224b9f52ac8bb15141a77d2
Lantianyou <dtlantianyou@gmail.com>
2020-09-10 20:37:59 +0800

    common

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,54 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "title",
+    price: 20,
+  });
+  await ticket.save();
+
+  return ticket;
+};
+
+it("fetches orders for an particular user", async () => {
+  // create 3 tickets
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+  // create one order as user 1
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  // create two orders as user 2
+
+  const { body: orderOne } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+
+  const { body: orderTwo } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
+  const { body: allBody } = await request(app)
+    .get("/api/orders")
+    .set("Cookie", userTwo)
+    .expect(200);
+
+  expect(allBody.length).toEqual(2);
+  expect(allBody[0].id).toEqual(orderOne.id);
+  expect(allBody[1].id).toEqual(orderTwo.id);
+  expect(allBody[0].ticket.id).toEqual(ticketTwo.id);
+  expect(allBody[1].ticket.id).toEqual(ticketThree.id);
+});
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from "express";
+import { Order, OrderStatus } from "../models/order";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from "@lanxtianyou/common";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Canclled;
+    await order.save();
 
-export { router as  deleteOrderRouter };
+    // publish an evnet
+
+    res.status(204).send(order);
+  }
+);
+
+export { router as deleteOrderRouter };
